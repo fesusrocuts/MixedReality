a4fd1a615c282755d08e8c217e33d910d6b33ff9
Fesus Rocuts <fesusrocuts@gmail.com>
2021-10-24 14:48:38 -0500

    AR my first project, p1

---
+++ AR/project1/Assets/ImageDetectorV1.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.ARFoundation;
+
+
+public class ImageDetectorV1 : MonoBehaviour
+{
+    private ARTrackedImageManager _arTrackedImageManager;
+
+    private void Awake()
+    {
+        _arTrackedImageManager = findObjectOfType<ARTrackedImageManager>();
+    }
+    
+    void OnEnable()
+    {
+        _arTrackedImageManager.trackedImagesChanges += OnImageChange;
+    }
+    
+    void OnDisable()
+    {
+        _arTrackedImageManager.trackedImagesChanges -= OnImageChange;
+    }
+    
+    void OnImageChange(ARTrackedImagesChangedEventArgs args)
+    {
+        foreach (var trackedImage in args.added)
+        {
+            Debug.Log(trackedImage.name);
+        }
+    }
+/*
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+*/
+}
